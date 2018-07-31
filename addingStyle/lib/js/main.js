b2562c87d76c21aa05485159c9549a67a38a8a78
aidanASC5 <aidan@smithbest.com>
2018-07-31 09:39:42 -0400

    7/31

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,5 @@
+let x = document.getElementsByClassName("right_side");
+x.item(0).style.backgroundColor="#d3c26f";
+
+let y = document.getElementsByClassName("main_container");
+y.item(0).style.textShadow= "-6px 5px 2px yellow";
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("click",reset1);
+
+function reset1(){
+document.getElementById('rengar').src="assets/rengar.png";
+alert("you've been deleted!");
+}
